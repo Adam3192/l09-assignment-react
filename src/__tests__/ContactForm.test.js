02f814984fbb4566712a3797781add8662fbd439
Adam3192 <jeremyadixon@hotmail.com>
2022-06-22 11:30:26 -0500

    lesson 9 assignment - react

--- src/__tests__/ContactForm.test.js
+++ src/__tests__/ContactForm.test.js
@@ -1,48 +1,52 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import ContactForm from '../ContactForm';
+import { fireEvent, render, screen } from '@testing-library/react'
+import ContactForm from '../ContactForm'
 
-beforeEach(() => {
- render(<ContactForm />)
+afterEach(() => {
+  jest.restoreAllMocks()
 })
 
 it('renders a Add Contact button', () => {
- expect(screen.getByText('Add Contact')).toBeInTheDocument();
-})
-
-afterEach(() => {
- jest.restoreAllMocks()
+  render(<ContactForm />)
+  expect(screen.getByText('Add Contact')).toBeInTheDocument()
 })
 
 it('renders a Name form field', () => {
- render(<ContactForm />)
- expect(screen.getByRole('textbox', { name: "Name Name" })).toBeInTheDocument()
+  render(<ContactForm />)
+  expect(screen.getByRole('textbox', { name: 'Name' })).toBeInTheDocument()
 })
 
 it('renders a Email form field', () => {
- render(<ContactForm />)
- expect(screen.getByRole('textbox', { name: "Email Email" })).toBeInTheDocument()
+  render(<ContactForm />)
+  expect(screen.getByRole('textbox', { name: 'Email' })).toBeInTheDocument()
 })
 
 it('renders a Phone form field', () => {
- render(<ContactForm />)
- expect(screen.getByRole('textbox', { name: "Phone Phone" })).toBeInTheDocument()
+  render(<ContactForm />)
+  expect(screen.getByRole('textbox', { name: 'Phone' })).toBeInTheDocument()
 })
 
 it('verifies form value is updated when typing', () => {
- const input = screen.getByRole('textbox', { name: "Name" })
- fireEvent.change(input, { target: { value: 'John' } })
- expect(input.value).toBe('John')
-})
-
-afterEach(() => {
- jest.restoreAllMocks()
+  render(<ContactForm />)
+  const input = screen.getByRole('textbox', { name: 'Name' })
+  fireEvent.change(input, { target: { value: 'John' } })
+  expect(input.value).toBe('John')
 })
 
 it('verifies callback function invoked when button clicked', () => {
- window.alert = jest.fn()
- render(<ContactForm />)
- const button = screen.getByRole('button', { name: "Add Contact" })
- fireEvent.click(button)
- expect(window.alert).toHaveBeenCalledTimes(1)
+  let onContactAdded = jest.fn()
+  render(<ContactForm onContactAdded={onContactAdded} />)
+  const button = screen.getByRole('button', { name: 'Add Contact' })
+  fireEvent.click(button)
+  expect(onContactAdded).toHaveBeenCalledTimes(1)
+})
+
+it('renders without error', () => {
+  render(<ContactForm />)
+})
+
+it('should match the snapshot', () => {
+  const { asFragment } = render(<ContactForm />)
+  const html = asFragment()
+  expect(html).toMatchSnapshot()
 })
 
